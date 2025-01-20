98734f4237db6221cfc8663ff91eb2f60bb4a32a
Gabrielle <ms.grodarte@gmail.com>
2025-01-20 13:47:17 -0800

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        return [word for word in list if (sorted([letter for letter in self.word]) == sorted([letter for letter in word]))]
